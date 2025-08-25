9a53f1da15bf7855e1a331631e29ccb1f30f255c
SK <saiki1030kento@icloud.com>
2025-08-25 13:34:08 +0900

    ホームバックとミックスバックに限定し，説明変数を整理

--- Case2_all.py
+++ Case2_all.py
@@ -238,7 +238,7 @@ train_files = [f for f in all_csv_files] # if f != simulation_target_file
 print(f"学習データ数: {len(train_files)}試合")
 #1: シミュレーション対象をファイルパスで指定 ###
 
-product_ids = ['ミックスバック自由席','ホームサポーター自由席'] #,"ホームバック自由席"
+product_ids = ['ミックスバック自由席','ホームサポーター自由席',"ホームバック自由席"] #,"ホームバック自由席"
 feature_cols = ['販売価格', '残り日数', "相手ランク"] #, "相手ランク", "順位"
 REGULARIZATION = 0.0001
 
@@ -250,7 +250,7 @@ for file in train_files:
         df = pd.read_csv(file, encoding='utf-8-sig')
         df['試合名'] = os.path.basename(file).replace('.csv', '')
         df['席種'] = df['席種'].str.replace(r'^\d+:\s*', '', regex=True)
-        df = df[df['席種'].str.contains("ミックスバック自由席|ホームサポーター自由席", na=False)].copy() #席種の限定|ホームバック自由席
+        df = df[df['席種'].str.contains("ミックスバック自由席|ホームサポーター自由席|ホームバック自由席", na=False)].copy() #席種の限定
         df = df.iloc[6:].copy() # インデックス6から最後までを使用
         train_df_list.append(df)
 df_train = pd.concat(train_df_list, ignore_index=True)
@@ -290,7 +290,7 @@ for simulation_target_file in all_csv_files:
     df_test = pd.read_csv(simulation_target_file, encoding='utf-8-sig')
     df_test['試合名'] = testmatch_name
     df_test['席種'] = df_test['席種'].str.replace(r'^\d+:\s*', '', regex=True)
-    df_test = df_test[df_test['席種'].str.contains("ミックスバック自由席|ホームサポーター自由席", na=False)].copy() #席種の限定|ホームバック自由席
+    df_test = df_test[df_test['席種'].str.contains("ミックスバック自由席|ホームサポーター自由席|ホームバック自由席", na=False)].copy() #席種の限定
     df_test = df_test.dropna(subset=feature_cols + ['販売数量']+['経過日数']) #変数追加
     print(f"シミュレーションデータの総件数: {len(df_test)}件")
     print(f"\n--- 『{testmatch_name}』の販売シミュレーション開始 ---")
@@ -367,8 +367,8 @@ for simulation_target_file in all_csv_files:
         # 到着分布は学習データから作られたものを使用
         daily_arrival_prob = estimator.arrival_distribution.get(day, 0)
         daily_arrivals = initial_arrivals_for_sim * daily_arrival_prob
-        
-        df_predict_day = day_data[['席種'] + feature_cols]
+
+        df_predict_day = day_data[['席種'] + ['経過日数'] + feature_cols]
         predicted_probas = estimator.predict_probas(df_predict_day)
 
         total_purchase_prob = sum(prob for prod, prob in predicted_probas.items() if prod != 'no_purchase')
